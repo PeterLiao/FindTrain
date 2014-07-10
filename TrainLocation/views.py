bdebb2c80f8ba16c7d9880ee6b7d55ff26f5e40b
peter_c_liao <s9356016@gmail.com>
2014-07-10 11:58:24 +0800

    Refine UI

--- TrainLocation/views.py
+++ TrainLocation/views.py
@@ -60,7 +60,7 @@ def show_train_schedule(request, train_id):
                                              "station_list": station_list,
                                              "train": train,
                                              "end_station": end_station,
-                                             "running_schedule:": running_schedule,
+                                             "running_schedule": running_schedule,
                                              "direction_id": train.direction})
 
 
